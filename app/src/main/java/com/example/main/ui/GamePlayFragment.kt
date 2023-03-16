eafa3b9b678a078a247b5666a3bbbcf3288e6280
Logan Shuler <55895177+nozoko@users.noreply.github.com>
2023-03-16 15:54:08 -0700

    Added functionality to change number of buttons displayed based on difficulty

--- app/src/main/java/com/example/main/ui/GamePlayFragment.kt
+++ app/src/main/java/com/example/main/ui/GamePlayFragment.kt
@@ -1,12 +1,18 @@
 package com.example.main.ui
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceManager
 import com.example.main.R
 
 class GamePlayFragment : Fragment(R.layout.game_fragment) {
@@ -14,6 +20,14 @@ class GamePlayFragment : Fragment(R.layout.game_fragment) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
 
+        setDifficulty(view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        view?.let { view ->
+            setDifficulty(view)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
@@ -28,6 +42,25 @@ class GamePlayFragment : Fragment(R.layout.game_fragment) {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setDifficulty(view: View) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val difficulty = prefs.getString(getString(R.string.pref_difficulty_key), getString(R.string.pref_difficulty_default_value))?:"not found"
+        val button3 = view.findViewById<Button>(R.id.button_3)
+        val button4 = view.findViewById<Button>(R.id.button_4)
+        if (difficulty == getString(R.string.pref_difficulty_value_easy)) {
+            button3.visibility = INVISIBLE
+            button4.visibility = INVISIBLE
+        }
+        else if (difficulty == getString(R.string.pref_difficulty_value_medium)) {
+            button3.visibility = VISIBLE
+            button4.visibility = INVISIBLE
+        }
+        else {
+            button3.visibility = VISIBLE
+            button4.visibility = VISIBLE
         }
     }
 }
