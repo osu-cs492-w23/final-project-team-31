2608c50493109d5cb09288cef3078d41092fceca
Logan Shuler <55895177+nozoko@users.noreply.github.com>
2023-03-15 22:01:50 -0700

    Basic functionality done, pushing and merging to continue work with templates

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfig: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(
+                R.id.nav_host_fragment
+            ) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(
+            navController,
+            appBarConfig
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController =
+            findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/main/ui/HomeScreenFragment.kt
@@ -0,0 +1,7 @@
+package com.example.main.ui
+
+import androidx.fragment.app.Fragment
+import com.example.main.R
+
+class HomeScreenFragment : Fragment(R.layout.PLACEHOLDER) {
+}
