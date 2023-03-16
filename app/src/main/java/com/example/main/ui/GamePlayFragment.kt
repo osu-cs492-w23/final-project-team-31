72c565de7267e6795d1e0b62a2a50d97938812df
Logan Shuler <55895177+nozoko@users.noreply.github.com>
2023-03-15 22:45:32 -0700

    Basic screens and navigation functionality done (app compiles and runs)

--- app/src/main/java/com/example/main/ui/GamePlayFragment.kt
+++ app/src/main/java/com/example/main/ui/GamePlayFragment.kt
@@ -1,8 +1,33 @@
 package com.example.main.ui
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.main.R
 
-class GamePlayFragment : Fragment(R.layout.PLACEHOLDER) {
+class GamePlayFragment : Fragment(R.layout.game_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.main_nav_drawer, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val directions = GamePlayFragmentDirections.navigateToSettings()
+                findNavController().navigate(directions)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
