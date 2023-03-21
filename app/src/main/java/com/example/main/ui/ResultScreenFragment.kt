e2daec46319c9bb6ba8f27932dad2e2473b8e5e5
Logan Shuler <55895177+nozoko@users.noreply.github.com>
2023-03-20 17:31:48 -0700

    Added randomization of button choices based on sample data, and proper calling of correct/incorrect screens

--- app/src/main/java/com/example/main/ui/ResultScreenFragment.kt
+++ app/src/main/java/com/example/main/ui/ResultScreenFragment.kt
@@ -5,14 +5,33 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.main.R
 
 class ResultScreenFragment : Fragment(R.layout.result_fragment) {
+    private var isCorrect: Boolean = false
+    private var correctAnswer: String = "placeholder"
+    private val args: ResultScreenFragmentArgs by navArgs()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        isCorrect = args.isCorrect
+        correctAnswer = args.correctAnswer
+
+        val correctView = view.findViewById<ImageView>(R.id.ic_guess_correct)
+        val incorrectView = view.findViewById<ImageView>(R.id.ic_guess_wrong)
+        if (isCorrect) {
+            correctView.visibility = VISIBLE
+            incorrectView.visibility = INVISIBLE
+        } else {
+            correctView.visibility = INVISIBLE
+            incorrectView.visibility = VISIBLE
+        }
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
 
